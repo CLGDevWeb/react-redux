a4d3d7b5e6b11bbd12aa17223b7ac022c546277b
unknown <clgdevweb@gmail.com>
2020-12-10 20:44:18 +0100

    Essais redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import '../src/styles/App.css';
+import {useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const login = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <p>Hello there</p>
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch({type: "INCREMENT"})} >Increment</button>
+      <button onClick={() => dispatch({type: "SIGN_IN"})} >Sign In</button>
+      {login && (
+        <p>Connected!</p>
+      )}
     </div>
   );
 }
